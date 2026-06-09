5b8bf12cfa2dc8e2f0e3e8a013cb90272679578c
katakura <c0c2501844@edu.teu.ac.jp>
2026-06-09 15:59:59 +0900

    練習7,8,9

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8：背景画像の左右反転
     kk_img = pg.image.load("fig/3.png")  # 練習3：こうかとんSurfaceの作成
     kk_img = pg.transform.flip(kk_img, True, False)  # 練習3：こうかとんの左右反転
     tmr = 0
@@ -18,17 +19,14 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        x = tmr
-        if x >= 1600:
-            x = 0
-            tmr = 0
-
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])  # 練習5：背景画像を右から左へ
-        screen.blit(bg_img, [-x + 1600, 0])  # 練習5：背景画像を右から左へ
+        screen.blit(bg_img2, [-x + 1600, 0])  # 練習7：2枚目Surface
+        screen.blit(bg_img, [-x + 3200, 0])  # 練習9：3枚目の背景画像の描画
         screen.blit(kk_img, [300, 200])  # 練習4：こうかとんSurfaceの描画
         pg.display.update()
-        tmr += 1
-        clock.tick(2000)
+        tmr += 10
+        clock.tick(200)
 
 
 if __name__ == "__main__":
