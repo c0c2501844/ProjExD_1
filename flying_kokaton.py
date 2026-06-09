538ca8c112c1f80f1904e5fea53142a1a2098886
katakura <c0c2501844@edu.teu.ac.jp>
2026-06-09 17:21:55 +0900

    演習2：修正

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -26,9 +26,11 @@ def main():
         kk_x = 0
         kk_y = 0
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP] or key_lst[pg.K_LEFT]:
+        if key_lst[pg.K_UP]:
             kk_y = -1
-        if key_lst[pg.K_DOWN] or key_lst[pg.K_RIGHT]:
+        if key_lst[pg.K_LEFT]:
+            kk_x = -2
+        if key_lst[pg.K_DOWN]:
             kk_y = +1
         if key_lst[pg.K_RIGHT]:
             kk_x = +3
@@ -36,7 +38,7 @@ def main():
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
         screen.blit(kk_img, kk_rct)
-        kk_rct.move_ip((kk_x, kk_y))
+        kk_rct.move_ip((kk_x - 1, kk_y))
         pg.display.update()
         tmr += 1
         clock.tick(200)
