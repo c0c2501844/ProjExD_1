a26b628437b2e7bb5dcb157c0ccf2c6c40f9981e
katakura <c0c2501844@edu.teu.ac.jp>
2026-06-09 16:11:59 +0900

    練習10：こうかとん移動ができるように

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8：背景画像の左右反転
     kk_img = pg.image.load("fig/3.png")  # 練習3：こうかとんSurfaceの作成
     kk_img = pg.transform.flip(kk_img, True, False)  # 練習3：こうかとんの左右反転
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,12 +22,17 @@ def main():
                 return
 
         x = tmr % 3200
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP] or key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN] or key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((0, +1))
         screen.blit(bg_img, [-x, 0])  # 練習5：背景画像を右から左へ
         screen.blit(bg_img2, [-x + 1600, 0])  # 練習7：2枚目Surface
         screen.blit(bg_img, [-x + 3200, 0])  # 練習9：3枚目の背景画像の描画
-        screen.blit(kk_img, [300, 200])  # 練習4：こうかとんSurfaceの描画
+        screen.blit(kk_img, kk_rct)  # 練習4：こうかとんSurfaceの描画
         pg.display.update()
-        tmr += 10
+        tmr += 1
         clock.tick(200)
 
 
